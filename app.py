44f6514a0816a0e625bcfd1b1a27c81b4ed18ad3
aishaaaS <aishasuleimanshaik.ai@gmail.com>
2026-03-19 01:25:36 +0530

    fix port for render

--- app.py
+++ app.py
@@ -176,4 +176,5 @@ def download_report(yield_value):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port=int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
